54699aaf0cabeee183d2a4e946e9699b526827a6
Sylvain LE GAL <sylvain.legal@grap.coop>
2024-07-23 15:35:03 +0200

    [FIX] Supplier CDS : allow discounts

--- grap_account_invoice_invoice2data_templates/tests/test_cds.py
+++ grap_account_invoice_invoice2data_templates/tests/test_cds.py
@@ -7,7 +7,7 @@ from .test_module import TestModule
 
 
 class TestCds(TestModule):
-    def test_cds(self):
+    def test_cds_01(self):
         self._test_supplier_template(
             "cds__2023-01-30__ACR__37136.pdf",
             line_qty=4,
@@ -35,6 +35,38 @@ class TestCds(TestModule):
                     "quantity": 2,
                     "price_unit": 73.93,
                     "price_subtotal": 147.86,
+                },
+            ],
+        )
+
+    def test_cds_02(self):
+        self._test_supplier_template(
+            "cds__2024-07-15__CRB__43181.pdf",
+            line_qty=8,
+            expected_values={
+                "issuer": "CDS",
+                "date": datetime(day=15, month=7, year=2024),
+                "invoice_number": "43181",
+                "amount_untaxed": 939.04,
+                "amount": 1126.85,
+            },
+            expected_lines=[
+                {
+                    "product_code": "25JE003BV",
+                    "product_name": "LESSIVE HYPO 20KG",
+                    "vat_code": "1",
+                    "quantity": 1,
+                    "price_unit": 47.55,
+                    "price_subtotal": 47.55,
+                },
+                {
+                    "product_code": "20JE003BV_3+1_P",
+                    "product_name": "LESSIVE FLEUR A SAVON 2 EN 1 - 20KG 3+1",
+                    "vat_code": "1",
+                    "quantity": 1,
+                    "quantity2": 3,
+                    "price_unit": 56.85,
+                    "price_subtotal": 170.55,
                 },
             ],
         )
